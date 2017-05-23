0d7171797afa278f60849a447c42cfddc253b0c0
Mary Morvan <mamorvan@yahoo.com>
2017-05-23 18:30:26 -0400

    take ds_store and node_module folders out

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,19 @@
 //set up code to connect Node to MySql
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-	port: 3306,
-	host: "localhost",
-	user: "root",
-	password: "BonnieMad1son",
-	database: "sushi_db"
-});
+var connection;
+
+if (process.env.JAWSDB_URL) {
+	connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+	connection = mysql.createConnection({
+		port: 3306,
+		host: "localhost",
+		user: "root",
+		password: "BonnieMad1son",
+		database: "sushi_db"
+	});
+};
 
 connection.connect(function(err){
 	if (err) {
