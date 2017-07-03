0a9e0f49c2eb19f121da8cf27bc52268e58e561d
Mary Morvan <mamorvan@yahoo.com>
2017-07-03 16:01:22 -0400

    remove local db passsword :lock:

--- config/connection.js
+++ config/connection.js
@@ -9,7 +9,7 @@ if (process.env.JAWSDB_URL) {
 	connection = mysql.createConnection({
 		host: "localhost",
 		user: "root",
-		password: "BonnieMad1son",
+		password: "",
 		database: "sushi_db"
 	});
 };
